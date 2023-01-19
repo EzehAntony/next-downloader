0c7dd71cb5521b14ba1d7d4184a55266e65c6eaa
Anthony <crayonno.o@gmail.com>
2023-01-19 12:50:02 +0100

    card component and background fix

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -3,14 +3,26 @@
 import React from "react";
 import styles from "./home.module.css";
 import { Ubuntu } from "@next/font/google";
+import Card from "@/components/card/Card";
 
 const ubuntu = Ubuntu({ weight: "500" });
 function home() {
+  const socialMedia = [
+    "instagram",
+    "whatsapp",
+    "facebook",
+    "twitter",
+    "tiktok",
+    "youtube",
+  ];
   return (
     <div className={styles.home}>
-      <h1 className={ubuntu.className}>
-        The social media cards would be displayed here
-      </h1>
+      <h1 className={ubuntu.className}></h1>
+      <div className={styles.socials}>
+        {socialMedia.map((e) => (
+          <Card data={e} />
+        ))}
+      </div>
     </div>
   );
 }
